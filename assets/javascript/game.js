d078fef6604fbaea75478c25e92649029168752b
kpassaglia <kpassaglia@gmail.com>
2019-04-01 16:49:00 -0500

    added function 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,18 @@
 //start game 
-
+initialize();
+function initialize(){
 
 const solve1 = document.getElementById("word");
 const wins = document.getElementById("wins");
 const remaining = document.getElementById("remaining");
 const badGuess = document.getElementById("badGuesses");
-
 const winCount = 0;
 const wordList = ["tottenham", "liverpool", "bournmouth", "newcastle", "arsenal"];
-wrongGuesses = [];
-correctGuesses = [];
-keyGuess = " ";
-const word = wordList[Math.floor(Math.random() * wordList.length)];
+let wrongGuesses = [];
+let correctGuesses = [];
+let keyGuess = " ";
+let word = wordList[Math.floor(Math.random() * wordList.length)];
+
 
 console.log(wordList);
 console.log(word);
@@ -26,9 +27,10 @@ for (var i = 0; i < word.length; i++) {
 };
 solve1.innerHTML = correctGuesses.join(' ');
 
+
 //on key event to parse where the response goes
 document.onkeyup = function (event) {
-    var keyGuess = event.key;
+    let keyGuess = event.key;
 
     if (word.indexOf(keyGuess) === -1) {
         wrongGuesses.push(keyGuess);
@@ -46,37 +48,18 @@ document.onkeyup = function (event) {
     console.log("right" + correctGuesses);
     console.log("wrong" + wrongGuesses);
 
+
     //finsish alerts
     if (correctGuesses.indexOf('_') === -1) {
         alert('You Won!!!');
         wins.innerHTML = "Wins: " + (winCount + 1)
+        initialize();
 
     }
     else if (12 - wrongGuesses.length == 0) {
         alert("You lost!!!");
-
+        initialize();
     }
 };
 
-function reset() {
-    
-    var wordList = ["tottenham", "liverpool", "bournmouth", "newcastle", "arsenal"];
-    var word = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(wordList);
-    console.log(word);
-    console.log(wrongGuesses);
-    console.log(correctGuesses);
-    for (var i = 0; i < word.length; i++) {
-        correctGuesses.push("_");
-    }
-        wrongGuesses = [];
-        correctGuesses = [];
-        keyGuess = " ";
-        badGuess.innerHTML = wrongGuesses;
-        for (var i = 0; i < word.length; i++) {
-            correctGuesses.push("_");
-            console.log(correctGuesses);
-            console.log(word.length);
-        };
-        solve1.innerHTML = correctGuesses.join(' ');
-    }
+};
